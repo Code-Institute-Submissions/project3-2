1972d9e59fa6de7a005ccda0ef6dc99041aad2ca
DimitarD <dimitardobrikov77@gmail.com>
2018-08-02 07:21:55 +0000

    Added requirements file

--- appread.py
+++ appread.py
@@ -6,7 +6,6 @@ app=Flask(__name__)
 @app.route('/')  
 def hello (): 
     return "Hello world"  
-    
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
